239186cff89bba992131c94934141048fc8e5af5
Prajwal-cmd1 <prajwal01K@gmail.com>
2024-12-18 09:18:06 +0530

    Update config.js

--- constants/config.js
+++ constants/config.js
@@ -1,7 +1,8 @@
 const corsOption = {
     origin:["http://localhost:5173","http://localhost:4173","https://knock-frontend.vercel.app",process.env.CLIENT_URL],
     methods:["GET","POST","PUT","DELETE"],
-    credentials:true
+    credentials: true,
+    transports: ['websocket', 'polling'],
 }
 
 
