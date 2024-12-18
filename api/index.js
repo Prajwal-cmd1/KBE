5d3eb62f51f97360ba940a01213161f3c275da67
Prajwal-cmd1 <prajwal01K@gmail.com>
2024-12-18 18:49:58 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -35,10 +35,15 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin:["http://localhost:5173","http://localhost:4173","https://knock-frontend.vercel.app",process.env.CLIENT_URL],
-    methods:["GET","POST","PUT","DELETE"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:4173",
+      "https://knock-frontend.vercel.app",
+      process.env.CLIENT_URL,
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
-}
+  },
 });
 
 app.set("io", io); // Access io in getSocket function in other file
@@ -51,9 +56,14 @@ const onlineUsers = new Set();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:["http://localhost:5173","http://localhost:4173","https://knock-frontend.vercel.app",process.env.CLIENT_URL],
-    methods:["GET","POST","PUT","DELETE"],
-    credentials: true,
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:4173",
+    "https://knock-frontend.vercel.app",
+    process.env.CLIENT_URL,
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
 }));
 
 app.use("/api/v1/user", userRouter);
